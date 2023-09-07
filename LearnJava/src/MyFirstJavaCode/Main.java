2cb3d89ef2cc5a10ae4eef7ed957e0b448a6301e
Ankit kumar Gupta <erankit0711@gmail.com>
2023-09-07 13:26:48 +0530

    Initial Commmit

---
+++ LearnJava/src/MyFirstJavaCode/Main.java
@@ -0,0 +1,19 @@
+package MyFirstJavaCode;
+
+class Gain{
+    public void func() {
+        System.out.println("function!");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Gain g1 = new Gain();
+        
+    }
+}
+
